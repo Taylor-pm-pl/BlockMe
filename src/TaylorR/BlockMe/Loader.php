a0de4373277bd5970cb3940c80f38cd044a96afe
Taylor.R <nguyenhieucoder47@gmail.com>
2023-05-15 00:53:54 +0700

    Init

--- src/TaylorR/BlockMe/Loader.php
+++ src/TaylorR/BlockMe/Loader.php
@@ -4,12 +4,34 @@ declare(strict_types=1);
 
 namespace TaylorR\BlockMe;
 
+use pocketmine\event\EventPriority;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\plugin\PluginBase;
 
 class Loader extends PluginBase implements Listener {
 
     protected function onEnable(): void
     {
+        $this->getServer()->getPluginManager()->registerEvent(
+            PlayerMoveEvent::class,
+            function (PlayerMoveEvent $event): void {
+                $player = $event->getPlayer();
+                if ($player->isSpectator()) return;
+                $viewers = $player->getViewers();
+                foreach ($viewers as $viewer) {
+                    $p_pos = $player->getPosition();
+                    $v_pos = $viewer->getPosition();
+                    $distance = 0.5;
+                    if ($p_pos->distance($v_pos) <= $distance) {
+                        $player->knockBack(
+                            $p_pos->getX() - $v_pos->getX(),
+                            $p_pos->getZ() - $v_pos->getZ(),
+                            0.1
+                        );
+                    }
+                }
+            }, EventPriority::NORMAL, $this
+        );
     }
 }
