267fef30b861c4f93bc8e00bc8b99046210663ea
Taylor.R <nguyenhieucoder47@gmail.com>
2023-05-15 00:19:57 +0700

    Construct...

--- README.md
+++
@@ -1,2 +0,0 @@
-# BlockMe
-Players can block each other
---
+++ src/TaylorR/BlockMe/Loader.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TaylorR\BlockMe;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Loader extends PluginBase implements Listener {
+
+    protected function onEnable(): void
+    {
+    }
+}
