4b4b1ca2ee61c7406edac6adb79b208d3267c339
Taylor.R <nguyenhieucoder47@gmail.com>
2023-05-15 01:09:08 +0700

    Update something

--- src/TaylorR/BlockMe/Loader.php
+++ src/TaylorR/BlockMe/Loader.php
@@ -10,28 +10,28 @@ use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\plugin\PluginBase;
 
 class Loader extends PluginBase implements Listener {
-
-    protected function onEnable(): void
-    {
-        $this->getServer()->getPluginManager()->registerEvent(
-            PlayerMoveEvent::class,
-            function (PlayerMoveEvent $event): void {
-                $player = $event->getPlayer();
-                $viewers = $player->getViewers();
-                if ($player->isSpectator()) return;
-                foreach ($viewers as $viewer) {
-                    $p_pos = $player->getPosition();
-                    $v_pos = $viewer->getPosition();
-                    $distance = 0.5;
-                    if ($p_pos->distance($v_pos) <= $distance) {
-                        $player->knockBack(
-                            $p_pos->getX() - $v_pos->getX(),
-                            $p_pos->getZ() - $v_pos->getZ(),
-                            0.1
-                        );
-                    }
-                }
-            }, EventPriority::NORMAL, $this
-        );
-    }
+	protected function onEnable() : void {
+		$this->getServer()->getPluginManager()->registerEvent(
+			PlayerMoveEvent::class,
+			function (PlayerMoveEvent $event) : void {
+				$player = $event->getPlayer();
+				$viewers = $player->getViewers();
+				if ($player->isSpectator()) {
+					return;
+				}
+				foreach ($viewers as $viewer) {
+					$p_pos = $player->getPosition();
+					$v_pos = $viewer->getPosition();
+					$distance = 0.5;
+					if ($p_pos->distance($v_pos) <= $distance) {
+						$player->knockBack(
+							$p_pos->getX() - $v_pos->getX(),
+							$p_pos->getZ() - $v_pos->getZ(),
+							0.1
+						);
+					}
+				}
+			}, EventPriority::NORMAL, $this
+		);
+	}
 }
